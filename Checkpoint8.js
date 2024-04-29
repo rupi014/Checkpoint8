211d48fa5b103a8b8d37c9c52e89664a8a07716d
Rubens Ballester Lillo <148860042+rupi014@users.noreply.github.com>
2024-04-29 17:08:10 +0200

    Update Checkpoint8.js

--- Checkpoint8.js
+++ Checkpoint8.js
@@ -1,7 +1,10 @@
-const miLista = ["velma", "exploradora", "jane", "john", "harry"]
+// 1. Cree un bucle for en JS que imprima cada nombre en esta lista. miLista = “velma”, “exploradora”, “jane”, “john”, “harry”
+// 2. Cree un bucle while que recorra la misma lista y también imprima los nombres. Nota: Recuerda crear un contador para que el ciclo no sea infinito.
+// 3. Cree una función de flecha que devuelva "Hola mundo".
 
 // Ejercicio 1. Bucle FOR
 
+const miLista = ["velma", "exploradora", "jane", "john", "harry"]
 for ( let i = 0; i < miLista.length; i++) {
     console.log(miLista[i])
 }
@@ -14,7 +17,7 @@ while (count < miLista.length) {
   count++
 }
 
-// Ejercicio 3. FUNCIÓN FECHA
+// Ejercicio 3. FUNCIÓN FLECHA
 
 const flecha = () => {
   console.log("Hola Mundo")
